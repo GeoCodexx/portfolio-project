8e764c77eef73ea54a290cf4c6c9d1b97c7fa427
unknown <lightningdeath2014@gmail.com>
2023-06-19 15:32:18 -0500

    Portfolio avanzado al 90%

---
+++ js/script.js
@@ -0,0 +1,83 @@
+/* TOGGLE ICON NAVBAR*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+});
+
+/*READ MORE BUTTON ABOUT */
+let p = document.querySelector("#about-readmore");
+let btnReadMore = document.querySelector("#readmore-btn");
+
+const readMore = () => {
+  if (btnReadMore.textContent == "Leer más...") {
+    p.style.display = "inline";
+    //console.log(p.style.display);
+    btnReadMore.textContent = "Leer menos...";
+  } else {
+    p.style.display = "none";
+    btnReadMore.textContent = "Leer más...";
+  }
+};
+btnReadMore.addEventListener("click", readMore);
+
+/* scroll SECTIONS ACTIVE LINK*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*STICKY NAVBAR */
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*remove  toggle icon y navbar when click link*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+/*SCROLL REVEAL */
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 1500,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/*TYPED.JS */
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Desarrollador Frontend",
+    "Desarrollador Backend",
+    "Diseñador Gráfico",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 800,
+  loop: true,
+});
